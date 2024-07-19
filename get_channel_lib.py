023ced60c36ae80625c2d02d0c97594004b612ef
byColton <colton.byers@gmail.com>
2024-07-19 13:41:53 -0400

    - Update to accept multiple channel names. - Added channel id to dataframe.

--- get_channel_lib.py
+++ get_channel_lib.py
@@ -2,10 +2,11 @@ import argparse
 import pandas as pd
 from pytubefix import YouTube, Channel
 
-def get_tube_lib(name):
-    '''This function takes a channel name and returns a pandas dataframe with the video id and title.'''
-
+def get_tube_lib(channel_names):
+    '''This function takes a channel names and returns a pandas dataframe with the channel name, video id, video title, description, and thumbnail url.'''
+    
     # create lists for the video id and title columns
+    channels = []
     ids = []
     titles = []
     view_counts = []
@@ -14,34 +15,41 @@ def get_tube_lib(name):
     # dictionary for reference
     video_dict = {}
 
-    c = Channel(f'https://www.youtube.com/@{name}')
+    for name in channel_names:
 
-    for video in c.videos:
-        ids.append(video.video_id)
-        titles.append(video.title)
-        desc.append(video.description)
-        thumbnail_url.append(video.thumbnail_url)
-        video_dict[video.video_id] = video.title
-
+        print(f'Getting videos information for {name}')
+    
+        c = Channel(f'https://www.youtube.com/@{name}')
+    
+        for video in c.videos:
+            print(f'Processing video information for {video.title}')
+            channels.append(video.author)
+            ids.append(video.video_id)
+            titles.append(video.title)
+            desc.append(video.description)
+            thumbnail_url.append(video.thumbnail_url)
+            video_dict[video.video_id] = video.title
     # build and return pandas df with ids and titles as the two columns
     df = pd.DataFrame({
-        'Video ID': ids,
-        'Title': titles,
-        'Description': desc,
-        'Thumbnails': thumbnail_url
+            'Channel Name':channels,
+            'Video ID': ids,
+            'Title': titles,
+            'Description': desc,
+            'Thumbnails': thumbnail_url
     })
 
     return df, video_dict
 
 def main():
     parser = argparse.ArgumentParser(description="Fetch video details from a YouTube channel.")
-    parser.add_argument('channel_name', type=str, help='The name of the YouTube channel.')
+    parser.add_argument('channel_names', nargs='+', help='The names of the YouTube channels. i.e. Channel1 Channel2')
 
     args = parser.parse_args()
-    channel_name = args.channel_name
+    channel_names = args.channel_names
 
-    df, video_dict = get_tube_lib(channel_name)
-    df.to_csv(f'{channel_name}_videos.csv', index=False)
+    df, video_dict = get_tube_lib(channel_names)
+    output_filename = '_'.join(channel_names) + '_videos.csv'
+    df.to_csv(output_filename, index=False)
     print(df)
 
 if __name__ == "__main__":
